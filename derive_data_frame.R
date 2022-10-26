855676262e143bf2a197c567936ca5849c5607aa
kyle <kealbert@ad.unc.edu>
2022-10-25 20:18:12 -0400

    Potentially finished data preprocessing before analysis

--- derive_data_frame.R
+++ derive_data_frame.R
@@ -1,10 +1,18 @@
 library(tidyverse)
 library(lubridate)
-setwd("/home/rstudio/work/source_data/")
-music <- read.csv("df_music.csv")
+music <- read.csv("/home/rstudio/work/source_data/df_music.csv")
 music <- music %>% mutate(Play = case_when(
   ms_played >= 30000 ~ TRUE,
   ms_played < 30000 ~ FALSE
 ))
 
+time_zone <- "America/New_York"
+music$ts <- lubridate::with_tz(
+  as.POSIXct(music$ts, format = "%Y-%m-%dT%H:%M:%OSZ", tz = "UTC"),
+  tz = time_zone)
 
+if(!dir.exists("/home/rstudio/work/derived_data/")){
+  dir.create("/home/rstudio/work/derived_data")
+}
+
+save(music, file="/home/rstudio/work/derived_data/music.Rda")
